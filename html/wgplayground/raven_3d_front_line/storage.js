15407fecec9402b7b2f27cd15d55133c511888c9
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 17:52:33 +0530

    Add offline WGPlayground games: Raven 3D - Front Line, Swap Color, Waterpark Sort

---
+++ html/wgplayground/raven_3d_front_line/storage.js
@@ -0,0 +1,67 @@
+async function storage(loadcallback){
+  await gp.player.ready;
+  if(window.ndk!=null){
+
+    ndk.getPlayer().then(player => {
+
+    player.getData().then(data => {
+		window.playerUID = player.getUniqueID();
+      console.log("data load");
+      console.log(data);
+
+      storage.get = function(key){
+        return data[key];
+      }
+      storage.set = function(key,value){
+        data[key] = value;
+      }
+
+      storage.push = function(){
+        player.setData(data).then(() => {
+          console.log('yandexsdk cloud push seccuss');
+        });
+      }
+
+      storage.type = 1;
+
+      storage.getraw = function(){
+        return data;
+      }
+
+      loadcallback();
+      console.log("storage bind yandexsdk cloud");
+    }).catch(err => {
+      lssave();
+    });
+
+    }).catch(err => {
+      lssave();
+   });
+  } else {
+    lssave();
+  }
+
+  function lssave(){
+    console.log("storage bind gamepush");
+    storage.get = function(key){
+      return gp.player.get(key);
+    }
+    storage.set = function(key,value){
+      gp.player.set(key,value);
+    }
+    storage.push = function(){
+      gp.player.sync();
+    }
+    storage.getraw = function(){
+      var obj = {};
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        obj[key] = value;
+      }
+      return obj;
+    }
+    storage.type = 0;
+    loadcallback();
+  }
+}
