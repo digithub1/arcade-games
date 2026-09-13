99b1dc3640ee209b7f35e32053eb44d927642290
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 16:33:50 +0530

    Add offline WGPlayground games: Heart Box, Favorite Puzzles, The Best Russian Billiards

---
+++ html/wgplayground/heart_box/scripts/c3main.js
@@ -0,0 +1,7 @@
+import "./../csv-parse-sync.js";
+import "./../csv-stringify-sync.js";
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/main.js";
+import "./project/scriptsInEvents.js";
