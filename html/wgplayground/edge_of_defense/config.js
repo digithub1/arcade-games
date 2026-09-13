9bc3bd622aabf38ef4ab7d26d1bd680ac4aa1e4a
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 17:47:59 +0530

    Add offline WGPlayground games: Artillery Time, Edge of Defense, Tile Hexa Sort

---
+++ html/wgplayground/edge_of_defense/config.js
@@ -0,0 +1,5 @@
+const shareRu = "Я играю в интересную казуальную оборонительную стрелялку с улучшенным оружием. Давай играть вместе!";
+const shareEn = "I'm playing an interesting casual defense shooting game with weapon upgrades. Join in!";
+
+const leadRu = "Игроки, которые продержались больше волн";
+const leadEn = "Players who survived more waves";
