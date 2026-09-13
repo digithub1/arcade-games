43e830e4a67af68b36f41e3fbb6195f56405d115
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 16:59:38 +0530

    Add offline WGPlayground games: Obby All Games: Dash or Die, Block Tree, Theme word search

---
+++ html/wgplayground/block_tree/src/settings.8e444.js
@@ -0,0 +1,20 @@
+window._CCSettings = {
+    platform: "fb-instant-games",
+    groupList: ["default", "Jieping"],
+    collisionMatrix: [
+        [true],
+        [false, false]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/Scene.fire",
+    orientation: "",
+    jsList: [],
+    bundleVers: {
+        internal: "803f8",
+        resources: "24f55",
+        main: "84112"
+    }
+};
