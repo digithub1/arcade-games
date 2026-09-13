f76af6787bcfa465d3024e3518dfeb10f7f4135b
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-14 00:38:26 +0530

    Add offline WGPlayground games (21 titles): space_adventure, space_attracts, space_bricks...

---
+++ html/wgplayground/special_holiday_solitaire/scripts/c3main.js
@@ -0,0 +1,6 @@
+import "./../lodash.custom.min.js";
+import "./../tween.js";
+import "./../parabolic.obf.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/scriptsInEvents.js";
