2cb6e01af5469870da784ea1087ac22e73e37d14
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 18:35:43 +0530

    Add offline WGPlayground games (6 titles): Austrian domino duel, Date the Difference, Bear Adventure, Crashy Chashy, Goal.io...

---
+++ html/wgplayground/speedrun_platformer/scripts/c3main.js
@@ -0,0 +1,6 @@
+import "./../xmlDom.js";
+import "./../xpath.js";
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./plugins/Eponesh_GameScore/c3runtime/main.js";
+import "./objRefTable.js";
