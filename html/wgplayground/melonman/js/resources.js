1929f936c13f86644a224582d83fb037fd5555b8
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-14 00:09:06 +0530

    Add offline WGPlayground games (24 titles): mazes_draw_the_path, mc8bit, mcbros_pixelcraft...

---
+++ html/wgplayground/melonman/js/resources.js
@@ -0,0 +1,382 @@
+game.resources = [
+	{
+		"name": "bgm-gameplay-malam",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "bgm-gameplay-pagi",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "bgm-gameplay-siang",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "bgm-mainmenu",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-ayam-idle",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-ayam-kehit",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-button",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-buy",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-char-jatuh",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-dog-kehit",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-error",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-gameend",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-kucing-kehit",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-lari",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-makan-buah",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-makan-cake",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-papan-kehit",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "sfx-pisang-keinjek",
+		"type": "audio",
+		"src": "data/audio/"
+	},
+	{
+		"name": "cashshop",
+		"type": "tmx",
+		"src": "data/level/cashshop.json"
+	},
+	{
+		"name": "charSelectLevel",
+		"type": "tmx",
+		"src": "data/level/charSelectLevel.json"
+	},
+	{
+		"name": "gamemode",
+		"type": "tmx",
+		"src": "data/level/gamemode.json"
+	},
+	{
+		"name": "level1",
+		"type": "tmx",
+		"src": "data/level/level1.json"
+	},
+	{
+		"name": "level2",
+		"type": "tmx",
+		"src": "data/level/level2.json"
+	},
+	{
+		"name": "loadDatabaseLevel",
+		"type": "tmx",
+		"src": "data/level/loadDatabaseLevel.json"
+	},
+	{
+		"name": "lobimulti",
+		"type": "tmx",
+		"src": "data/level/lobimulti.json"
+	},
+	{
+		"name": "melonpedia",
+		"type": "tmx",
+		"src": "data/level/melonpedia.json"
+	},
+	{
+		"name": "menu",
+		"type": "tmx",
+		"src": "data/level/menu.json"
+	},
+	{
+		"name": "multiplay",
+		"type": "tmx",
+		"src": "data/level/multiplay.json"
+	},
+	{
+		"name": "shop",
+		"type": "tmx",
+		"src": "data/level/shop.json"
+	},
+	{
+		"name": "stageselect",
+		"type": "tmx",
+		"src": "data/level/stageselect.json"
+	},
+	{
+		"name": "temp",
+		"type": "tmx",
+		"src": "data/level/temp.json"
+	},
+	{
+		"name": "melon_font",
+		"type": "image",
+		"src": "data/image/melon_font.png"
+	},
+	{
+		"name": "melon_font_y",
+		"type": "image",
+		"src": "data/image/melon_font_y.png"
+	},
+	{
+		"name": "melon_font",
+		"type": "binary",
+		"src": "data/fnt/melon_font.fnt"
+	},
+	{
+		"name": "melon_font_y",
+		"type": "binary",
+		"src": "data/fnt/melon_font_y.fnt"
+	},
+	{
+		"name": "charShopList",
+		"type": "json",
+		"src": "data/json/charShopList.json"
+	},
+	{
+		"name": "diamondlist",
+		"type": "json",
+		"src": "data/json/diamondlist.json"
+	},
+	{
+		"name": "outfitShopList",
+		"type": "json",
+		"src": "data/json/outfitShopList.json"
+	},
+	{
+		"name": "texture_buttonplay_0",
+		"type": "image",
+		"src": "data/image/texture_buttonplay_0.png"
+	},
+	{
+		"name": "texture_buttonplay_0",
+		"type": "json",
+		"src": "data/json/texture_buttonplay_0.json"
+	},
+	{
+		"name": "texture_button_0",
+		"type": "image",
+		"src": "data/image/texture_button_0.png"
+	},
+	{
+		"name": "texture_button_0",
+		"type": "json",
+		"src": "data/json/texture_button_0.json"
+	},
+	{
+		"name": "texture_finish_0",
+		"type": "image",
+		"src": "data/image/texture_finish_0.png"
+	},
+	{
+		"name": "texture_finish_0",
+		"type": "json",
+		"src": "data/json/texture_finish_0.json"
+	},
+	{
+		"name": "texture_gamemode_0",
+		"type": "image",
+		"src": "data/image/texture_gamemode_0.png"
+	},
+	{
+		"name": "texture_gamemode_0",
+		"type": "json",
+		"src": "data/json/texture_gamemode_0.json"
+	},
+	{
+		"name": "texture_LevelCharacter_0",
+		"type": "image",
+		"src": "data/image/texture_LevelCharacter_0.png"
+	},
+	{
+		"name": "texture_LevelCharacter_0",
+		"type": "json",
+		"src": "data/json/texture_LevelCharacter_0.json"
+	},
+	{
+		"name": "texture_LevelGameplayMulti_0",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplayMulti_0.png"
+	},
+	{
+		"name": "texture_LevelGameplayMulti_0",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplayMulti_0.json"
+	},
+	{
+		"name": "texture_LevelGameplay_0",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_0.png"
+	},
+	{
+		"name": "texture_LevelGameplay_0",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_0.json"
+	},
+	{
+		"name": "texture_LevelGameplay_1",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_1.png"
+	},
+	{
+		"name": "texture_LevelGameplay_1",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_1.json"
+	},
+	{
+		"name": "texture_LevelGameplay_2",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_2.png"
+	},
+	{
+		"name": "texture_LevelGameplay_2",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_2.json"
+	},
+	{
+		"name": "texture_LevelGameplay_3",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_3.png"
+	},
+	{
+		"name": "texture_LevelGameplay_3",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_3.json"
+	},
+	{
+		"name": "texture_LevelGameplay_4",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_4.png"
+	},
+	{
+		"name": "texture_LevelGameplay_4",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_4.json"
+	},
+	{
+		"name": "texture_LevelGameplay_5",
+		"type": "image",
+		"src": "data/image/texture_LevelGameplay_5.png"
+	},
+	{
+		"name": "texture_LevelGameplay_5",
+		"type": "json",
+		"src": "data/json/texture_LevelGameplay_5.json"
+	},
+	{
+		"name": "texture_LevelMainMenu_0",
+		"type": "image",
+		"src": "data/image/texture_LevelMainMenu_0.png"
+	},
+	{
+		"name": "texture_LevelMainMenu_0",
+		"type": "json",
+		"src": "data/json/texture_LevelMainMenu_0.json"
+	},
+	{
+		"name": "texture_LevelMelonpedia_0",
+		"type": "image",
+		"src": "data/image/texture_LevelMelonpedia_0.png"
+	},
+	{
+		"name": "texture_LevelMelonpedia_0",
+		"type": "json",
+		"src": "data/json/texture_LevelMelonpedia_0.json"
+	},
+	{
+		"name": "texture_LevelShop_0",
+		"type": "image",
+		"src": "data/image/texture_LevelShop_0.png"
+	},
+	{
+		"name": "texture_LevelShop_0",
+		"type": "json",
+		"src": "data/json/texture_LevelShop_0.json"
+	},
+	{
+		"name": "texture_LevelStageSelect_0",
+		"type": "image",
+		"src": "data/image/texture_LevelStageSelect_0.png"
+	},
+	{
+		"name": "texture_LevelStageSelect_0",
+		"type": "json",
+		"src": "data/json/texture_LevelStageSelect_0.json"
+	},
+	{
+		"name": "texture_lobimulti_0",
+		"type": "image",
+		"src": "data/image/texture_lobimulti_0.png"
+	},
+	{
+		"name": "texture_lobimulti_0",
+		"type": "json",
+		"src": "data/json/texture_lobimulti_0.json"
+	},
+	{
+		"name": "texture_menu_0",
+		"type": "image",
+		"src": "data/image/texture_menu_0.png"
+	},
+	{
+		"name": "texture_menu_0",
+		"type": "json",
+		"src": "data/json/texture_menu_0.json"
+	},
+	{
+		"name": "texture_pause_0",
+		"type": "image",
+		"src": "data/image/texture_pause_0.png"
+	},
+	{
+		"name": "texture_pause_0",
+		"type": "json",
+		"src": "data/json/texture_pause_0.json"
+	}
+];
