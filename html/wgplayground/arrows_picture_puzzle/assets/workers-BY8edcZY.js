c04da2a9196d4a159bbe0e2a5cbc464c337fb7f7
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 16:54:35 +0530

    Add offline WGPlayground games: Arrows: Picture Puzzle, Threads, Deep Drill Run

---
+++ html/wgplayground/arrows_picture_puzzle/assets/workers-BY8edcZY.js
@@ -0,0 +1,50 @@
+const a=`(function () {
+    'use strict';
+
+    const WHITE_PNG = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=";
+    async function checkImageBitmap() {
+      try {
+        if (typeof createImageBitmap !== "function") return false;
+        const response = await fetch(WHITE_PNG);
+        const imageBlob = await response.blob();
+        const imageBitmap = await createImageBitmap(imageBlob);
+        return imageBitmap.width === 1 && imageBitmap.height === 1;
+      } catch (_e) {
+        return false;
+      }
+    }
+    void checkImageBitmap().then((result) => {
+      self.postMessage(result);
+    });
+
+})();
+`;let e=null,i=class{constructor(){e||(e=URL.createObjectURL(new Blob([a],{type:"application/javascript"}))),this.worker=new Worker(e)}};i.revokeObjectURL=function(){e&&(URL.revokeObjectURL(e),e=null)};const o=`(function () {
+    'use strict';
+
+    async function loadImageBitmap(url, alphaMode) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(\`[WorkerManager.loadImageBitmap] Failed to fetch \${url}: \${response.status} \${response.statusText}\`);
+      }
+      const imageBlob = await response.blob();
+      return alphaMode === "premultiplied-alpha" ? createImageBitmap(imageBlob, { premultiplyAlpha: "none" }) : createImageBitmap(imageBlob);
+    }
+    self.onmessage = async (event) => {
+      try {
+        const imageBitmap = await loadImageBitmap(event.data.data[0], event.data.data[1]);
+        self.postMessage({
+          data: imageBitmap,
+          uuid: event.data.uuid,
+          id: event.data.id
+        }, [imageBitmap]);
+      } catch (e) {
+        self.postMessage({
+          error: e,
+          uuid: event.data.uuid,
+          id: event.data.id
+        });
+      }
+    };
+
+})();
+`;let t=null;class s{constructor(){t||(t=URL.createObjectURL(new Blob([o],{type:"application/javascript"}))),this.worker=new Worker(t)}}s.revokeObjectURL=function(){t&&(URL.revokeObjectURL(t),t=null)};export{i as W,s as a};
