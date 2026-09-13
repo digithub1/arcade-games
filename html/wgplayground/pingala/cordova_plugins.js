0796e99d5b75480837994d5d4781ceccfbabf677
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 17:10:16 +0530

    Add offline WGPlayground games: Pranker Simulator, The Flowers: Merge and Sell Bouquets, Pingala

---
+++ html/wgplayground/pingala/cordova_plugins.js
@@ -0,0 +1,44 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-insomnia/www/Insomnia.js",
+        "id": "cordova-plugin-insomnia.Insomnia",
+        "pluginId": "cordova-plugin-insomnia",
+        "clobbers": [
+            "window.plugins.insomnia"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-insomnia/src/browser/Insomnia.js",
+        "id": "cordova-plugin-insomnia.InsomniaProxy",
+        "pluginId": "cordova-plugin-insomnia",
+        "merges": [
+            "window.plugins.insomnia"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
+        "id": "cordova-plugin-screen-orientation.screenorientation",
+        "pluginId": "cordova-plugin-screen-orientation",
+        "clobbers": [
+            "cordova.plugins.screenorientation",
+            "screen.orientation"
+        ]
+    },
+    {
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "id": "es6-promise-plugin.Promise",
+        "pluginId": "es6-promise-plugin",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-insomnia": "4.3.0",
+    "cordova-plugin-screen-orientation": "3.0.4",
+    "cordova-plugin-browsersync-gen2": "1.1.7",
+    "es6-promise-plugin": "4.2.2"
+}
+// BOTTOM OF METADATA
+});
