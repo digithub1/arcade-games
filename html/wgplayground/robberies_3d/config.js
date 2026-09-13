6f7d8cdac4bbfb43c2cfe0436b443f20a8689818
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-13 17:51:42 +0530

    Add offline WGPlayground games: Robberies 3D, Tap Race Duel, Drop People

---
+++ html/wgplayground/robberies_3d/config.js
@@ -0,0 +1,5 @@
+const shareRu = "Ограбь банк прямо сейчас в онлайн игре про ограбления!";
+const shareEn = "Rob a bank right now in the online robbery game";
+
+const leadRu = "Лучшие грабители";
+const leadEn = "The best robbers";
